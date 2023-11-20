58238348078e79185c38363e8f687e54bafa2d1b
Oliver Götz <olivergoetz@Olivers-MacBook-Air-2022.local>
2023-11-20 17:33:48 +0100

    init commit

--- infrastructure/vercel.tf
+++ infrastructure/vercel.tf
@@ -1,8 +1,8 @@
 
 resource "vercel_project" "novotherm" {
-  name      = "novotherm"
-  framework = "nextjs"
-
+  name                       = "novotherm"
+  framework                  = "nextjs"
+  serverless_function_region = "fra1"
   git_repository = {
     type = "github"
     repo = data.github_repository.novotherm.full_name
