7f06848b0148b158702a0fbc4d99f4c63ec06349
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2024-01-03 22:15:21 +0100

    init release

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { Raleway } from "next/font/google";
 import { Footer } from "@/components/footer";
 import CookieConsent from "@/components/cookieConsent";
 
-import Head from "next/head";
 const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -54,19 +53,6 @@ export default function RootLayout({
 }) {
   return (
     <html className="scroll-smooth" lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-      </Head>
       <body className={raleway.className}>
         <Navbar />
         {children}
