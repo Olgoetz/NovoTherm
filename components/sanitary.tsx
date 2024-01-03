8bde4b6386fa49f4ceeeb9e55fd31773cb675c70
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2024-01-03 22:09:59 +0100

    unnecessary packages removed

--- components/sanitary.tsx
+++ components/sanitary.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { CheckCircle2 } from "lucide-react";
-import PhotoAlbum from "react-photo-album";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import ContactButton from "./contactButton";
